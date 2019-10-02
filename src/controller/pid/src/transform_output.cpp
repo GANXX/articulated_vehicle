ca8a7680b2c059c849b2a2e1c2437054dc9de814
GANXX <gaxx2280@live.com>
2019-10-02 13:37:02 +0800

    改成了四轮独驱动的模式 这样rviz中才能正常显示

--- src/controller/pid/src/transform_output.cpp
+++ src/controller/pid/src/transform_output.cpp
@@ -11,7 +11,9 @@ namespace sp
      
 // 创建一个消息类型 setpoint
   std_msgs::Float64 left_wheel_v;
+  std_msgs::Float64 left_wheel_v_behind;
   std_msgs::Float64 right_wheel_v;
+  std_msgs::Float64 right_wheel_v_behind;
   std_msgs::Float64 articulate_v;
   pid::wheel_vel wheel_v;
  //设置铰接车长度宽度 
@@ -26,6 +28,8 @@ namespace sp
     double YawRate = 0;
     double setpoint_left = 0;
     double setpoint_right = 0;
+    double setpoint_left_behind = 0;
+    double setpoint_right_behind = 0;
 }
 
 using namespace sp;
@@ -60,17 +64,29 @@ void pubVelocity(){
 	  LRl = Rf + VEHICLE_WIDTH/2; //计算左轮实际速度用
 	  setpoint_left = RobotV*(LRl/Rf);
 	  setpoint_right = RobotV*(LRr/Rf);
+	  //setpoint_left_behind =  setpoint_left ;
+	  //setpoint_right_behind = setpoint_right;
+
+	  double Rr = (lr*cos(articulate_angle)+lf)/sin(articulate_angle);
+	  LRr = Rr - VEHICLE_WIDTH/2; //计算右轮实际速度用
+	  LRl = Rr + VEHICLE_WIDTH/2; //计算左轮实际速度用
+	  setpoint_left_behind = RobotV*(LRl/Rr);
+	  setpoint_right_behind = RobotV*(LRr/Rr);
 
    }
    else {
 	setpoint_left = RobotV;
 	setpoint_right = RobotV;
+	setpoint_left_behind =  setpoint_left ;
+	setpoint_right_behind = setpoint_right;
    }
 	//
 	//
-	left_wheel_v.data      =  setpoint_left/WHEEL_RADIUS;
-	right_wheel_v.data     =  setpoint_right/WHEEL_RADIUS;
-	articulate_v.data      = - YawRate;
+	left_wheel_v.data         = setpoint_left/WHEEL_RADIUS;
+	left_wheel_v_behind.data  = setpoint_left_behind/WHEEL_RADIUS;
+	right_wheel_v.data        = setpoint_right/WHEEL_RADIUS;
+	right_wheel_v_behind.data = setpoint_right_behind/WHEEL_RADIUS;
+	articulate_v.data         = - YawRate;
 	
 	wheel_v.left_vel.data  = left_wheel_v.data;
 	wheel_v.right_vel.data = right_wheel_v.data;
@@ -94,8 +110,10 @@ int main(int argc, char** argv)
   ros::Subscriber subcar = n.subscribe("cmd_vel_for_control", 1000, getcarcontrolCB);
   ros::Subscriber subangle = n.subscribe("/xbot/joint_states",1000, getArticulateAngleCB);
 //创建一个话题发布
-  ros::Publisher setpoint_pub_l   = n.advertise<std_msgs::Float64>("left_wheel_v", 1000);
-  ros::Publisher setpoint_pub_r   = n.advertise<std_msgs::Float64>("right_wheel_v", 1000);
+  ros::Publisher setpoint_pub_l   = n.advertise<std_msgs::Float64>("left_wheel_v_front", 1000);
+  ros::Publisher setpoint_pub_lb   = n.advertise<std_msgs::Float64>("left_wheel_v_behind", 1000);
+  ros::Publisher setpoint_pub_r   = n.advertise<std_msgs::Float64>("right_wheel_v_front", 1000);
+  ros::Publisher setpoint_pub_rb   = n.advertise<std_msgs::Float64>("right_wheel_v_behind", 1000);
   ros::Publisher articulate_pub_v = n.advertise<std_msgs::Float64>("articulate_v", 1000);
   ros::Publisher setpoint_pub     = n.advertise<pid::wheel_vel>("wheel_v", 1000);
 
@@ -105,7 +123,9 @@ int main(int argc, char** argv)
     ros::spinOnce();
 	pubVelocity();
     setpoint_pub_l.publish(left_wheel_v);  
+    setpoint_pub_lb.publish(left_wheel_v_behind);  
     setpoint_pub_r.publish(right_wheel_v);
+    setpoint_pub_rb.publish(right_wheel_v_behind);
     articulate_pub_v.publish(articulate_v);
     setpoint_pub.publish(wheel_v);
 
